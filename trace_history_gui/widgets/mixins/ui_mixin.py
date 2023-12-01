b1b9d1ad474b807ac10386a5b2b1505d056181ab
Nick Lalic <nick.lalic@gmail.com>
2023-11-30 22:24:19 -0700

    ♻️  UiMixin __init__ should pass all args, kwargs

--- trace_history_gui/widgets/mixins/ui_mixin.py
+++ trace_history_gui/widgets/mixins/ui_mixin.py
@@ -2,8 +2,8 @@ def create_ui_mixin(ui_class, base_class):
 
     class UiMixin(base_class):
 
-        def __init__(self, parent=None):
-            base_class.__init__(self, parent)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
             self.ui = ui_class()
             self.ui.setupUi(self)
 
